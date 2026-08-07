b5bb9c082fdd6c89e11a54f1e6c4722849dba95e
Exorium <antoinedupret2012@gmail.com>
2026-08-07 14:28:02 +0200

    Refactored _setup_hand()

--- scenes/two_handed_webbing_system.gd
+++ scenes/two_handed_webbing_system.gd
@@ -18,25 +18,16 @@ signal web_started(hand: Hand)
 ## Emitted when web finishes
 signal web_finished(hand: Hand)
 
-## web state
-enum WebState {
-	IDLE,			## web is idle
-	FIRED,			## web is fired
-	WINCHING,		## web is winching
-}
-
 enum Hand {
 	LEFT,
 	RIGHT
 }
-
 
 ## Default web collision mask of 1-5 (world)
 const DEFAULT_COLLISION_MASK := 0b0000_0000_0000_0000_0000_0000_0001_1111
 
 ## Default web enable mask of 5:web-target
 const DEFAULT_ENABLE_MASK := 0b0000_0000_0000_0000_0000_0000_0001_0000
-
 
 ## Order in which movement is processed
 @export var order: int = 20
@@ -57,6 +48,9 @@ const DEFAULT_ENABLE_MASK := 0b0000_0000_0000_0000_0000_0000_0001_0000
 ## Winch speed applied to the player while the web is held
 @export_custom(PROPERTY_HINT_NONE, "suffix:m/s") var winch_speed := 2.0
 
+@export var pull_strength := 1
+@export var min_pull_distance : float = 0.10
+
 ## Probably need to add export variables for line size, maybe line material at
 ## some point so dev does not need to make children editable to do this.
 ## For now, right click on web node and make children editable to edit these
@@ -68,25 +62,15 @@ const DEFAULT_ENABLE_MASK := 0b0000_0000_0000_0000_0000_0000_0001_0000
 
 ## Input action that triggers webbing movement. Be sure this button does not
 ## conflict with other functions.
-@export var web_button_action := "trigger_click"
-
-## Hand offset to apply based on our controller pose
-## You can use auto if you're using the default aim_pose or grip_pose poses.
-#@export_enum("auto", "aim", "grip", "palm", "disable") var hand_offset_mode: int = 0:
-#		set(value):
-#			hand_offset_mode = value
-#			notify_property_list_changed()
-#			if is_inside_tree():
-#				_update_transform()
+@export var web_shoot_action := "trigger_click"
+@export var web_pulling_action := "grip_click"
 
 ## Class that handle all variables for hands
 class WebHand:
-	# What and is what
-	var side: Hand
+	var side: Hand #what hand is this WebHand
 	
 	# Controller related ref
 	var controller: XRController3D #The controller
-	var controller_tracker_and_pose := ""
 	
 	# Web related ref
 	var root: Node3D
@@ -97,7 +81,8 @@ class WebHand:
 	
 	# State ref
 	var active := false #Is this hand active
-	var web_button := false #Is the button pressed on the controller
+	var web_shoot_button := false #Is the button pressed on the controller
+	var web_pull_button := false #Is the putton for pulling the player is active
 	
 	# Hook related variables
 	var hook_object: Node3D = null
@@ -110,9 +95,6 @@ var right_hand := WebHand.new()
 func _enter_tree() -> void:
 	left_hand.controller = XRHelpers.get_left_controller(self)
 	right_hand.controller = XRHelpers.get_right_controller(self)
-	
-	#_update_transform()
-
 
 # Runs when node is added to scene
 func _ready() -> void:
@@ -122,43 +104,46 @@ func _ready() -> void:
 	# Skip if running in the editor
 	if Engine.is_editor_hint():
 		return
-		
-	# Setup side for hands
-	left_hand.side = Hand.LEFT
-	right_hand.side = Hand.RIGHT
 	
-	# Line creation nodes
-	left_hand.line_helper = $Left/LeftLineHelper
-	left_hand.line = $Left/LeftLineHelper/Line
-	right_hand.line_helper = $Right/RightLineHelper
-	right_hand.line = $Right/RightLineHelper/Line
-	
-	# Get Raycast node
-	left_hand.web_raycast = $Left/Left_Web_RayCast
-	right_hand.web_raycast = $Right/Right_Web_RayCast
-	
-	# Get web Target Node
-	left_hand.web_target = $Left/Left_Web_Target
-	right_hand.web_target = $Right/Right_Web_Target
-	
-	#Define the root
-	left_hand.root = $Left
-	right_hand.root = $Right
-	
-	# Reparent the root so it follow the hands
-	left_hand.root.reparent(left_hand.controller, true)
-	right_hand.root.reparent(right_hand.controller, true)
+	_setup_hand(left_hand, Hand.LEFT)
+	_setup_hand(right_hand, Hand.RIGHT)
 	
 	# Ensure web length is valid
 	var min_hook_length := 1.5 * XRServer.world_scale
 	if web_length < min_hook_length:
 		web_length = min_hook_length
+
+
+func _setup_hand(hand : WebHand, side: Hand):
+	var sideName: String
+	var sidePath: String
 	
-	_setup_hand(left_hand)
-	_setup_hand(right_hand)
-
-
-func _setup_hand(hand : WebHand):
+	match side:
+		#LEFT HAND
+		0:
+			sideName = "Left"
+			sidePath = "Left/"
+		#RIGHT HAND
+		1:
+			sideName = "Right"
+			sidePath = "Right/"
+	
+	# Setup side for hands
+	hand.side = side
+	
+	# Line creation nodes
+	hand.line_helper = get_node(sidePath+"LineHelper")
+	hand.line = get_node(sidePath+"LineHelper/Line")
+	
+	# Get Raycast node
+	hand.web_raycast = get_node(sidePath+"Web_RayCast")
+	
+	# Get web Target Node
+	hand.web_target = get_node(sidePath+"Web_Target")
+	
+	#Define the root
+	hand.root = get_node(sideName)
+	
 	# Set ray-cast
 	hand.web_raycast.target_position = Vector3(0, 0, -web_length) * XRServer.world_scale
 	hand.web_raycast.collision_mask = web_collision_mask
@@ -167,21 +152,8 @@ func _setup_hand(hand : WebHand):
 	hand.line.radius = rope_width
 	hand.line.hide()
 	
-	match hand.side:
-		0:
-			print("Left Hand")
-		1:
-			print("Right Hand")
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(_delta: float) -> void:
-#	# If we have a controller, make sure our hand transform is updated when needed.
-#	if _left_controller:
-#		var tracker_and_pose: String = _left_controller.tracker + "." + _left_controller.pose
-#		if _left_controller_tracker_and_pose != tracker_and_pose:
-#			_left_controller_tracker_and_pose = tracker_and_pose
-#			if hand_offset_mode == 0:
-#				_update_transform()
+	# Reparent the root so it follow the hands
+	hand.root.reparent(hand.controller, true)
 
 # Update the webbing line and target
 func _physics_process(_delta: float) -> void:
@@ -198,8 +170,6 @@ func _physics_process(_delta: float) -> void:
 
 ## If pointing web at target then show the target
 func _calculate_target_visibility(hand : WebHand):
-	print(hand.web_raycast.is_colliding())
-	print(hand.web_raycast.get_collider())
 	
 	if enabled and not hand.active and _is_raycast_valid(hand):
 		hand.web_target.global_transform.origin = hand.web_raycast.get_collision_point()
@@ -234,20 +204,6 @@ func _get_configuration_warnings() -> PackedStringArray:
 
 	# Return warnings
 	return warnings
-
-
-# Check property config
-#func _validate_property(property: Dictionary) -> void:
-#	if hand_offset_mode != 4 and (
-#			property.name == "position"
-#			or property.name == "rotation"
-#			or property.name == "scale"
-#			or property.name == "rotation_edit_mode"
-#			or property.name == "rotation_order"
-#	):
-#		# We control these, don't let the user set them.
-#		property.usage = PROPERTY_USAGE_NONE
-
 
 ## Adds support for [method is_xr_class] on XRTools classes
 func is_xr_class(xr_name: String) -> bool:
@@ -288,8 +244,6 @@ func physics_movement(
 	# Perform exclusive movement as we have dealt with gravity
 	player_body.velocity = player_body.move_player(player_body.velocity)
 	
-	print("This web is being used : " + name)
-	
 	return true
 
 func _can_use_web(disabled: bool, hand: WebHand) -> bool:
@@ -303,30 +257,29 @@ func _can_use_web(disabled: bool, hand: WebHand) -> bool:
 ## update the hand, if the button is pressed then update the web line
 func _update_hand(hand: WebHand) -> bool:
 	# Update web button
-	var old_button := hand.web_button
-	hand.web_button = hand.controller.is_button_pressed(web_button_action)
+	var old_web_shoot_button := hand.web_shoot_button
+	hand.web_shoot_button = hand.controller.is_button_pressed(web_shoot_action)
+	
+	var old_web_pull_button = hand.web_pull_button
+	hand.web_pull_button = hand.controller.is_button_pressed(web_pulling_action)
 	
 	# Enable/disable webbing
 	var do_impulse := false
-	if hand.active and not hand.web_button:
+	if hand.active and not hand.web_shoot_button:
 		_set_webbing(hand, false)
-	elif hand.web_button and not old_button and _is_raycast_valid(hand):
+	elif hand.web_shoot_button and not old_web_shoot_button and _is_raycast_valid(hand):
 		hand.hook_object = hand.web_raycast.get_collider()
 		hand.hook_point = hand.web_raycast.get_collision_point()
 		hand.hook_local = hand.hook_point * hand.hook_object.global_transform
 		
 		do_impulse = true
 		_set_webbing(hand, true)
-		
-	#print("----------------")
-	#print("Pressed :", hand.web_button)
-	#print("Old :", old_button)
-	#print("Raycast :", _is_raycast_valid(hand))
+	
 	return do_impulse
 
 ## Return a Vector3 that is the force that need to be applied to the player
 func _get_web_force(hand: WebHand, player_velocity: Vector3, do_impulse: bool) -> Vector3:
-	if not hand.active:
+	if not hand.active or not hand.web_pull_button:
 		return Vector3.ZERO
 	
 	# Get hook direction
@@ -339,13 +292,13 @@ func _get_web_force(hand: WebHand, player_velocity: Vector3, do_impulse: bool) -
 	var speed := impulse_speed if do_impulse else winch_speed
 	if hook_length < 1.0:
 		speed = 0.0
-
+	
 	# Ensure velocity is at least winch_speed towards hook
 	var vdot: float = player_velocity.dot(hook_direction)
 	if vdot < speed:
+		print("Pulled Force : " + str(hook_direction * (speed - vdot)))
 		return hook_direction * (speed - vdot)
 	
-	print("GET WEB FORCE")
 	return Vector3.ZERO
 
 
@@ -370,7 +323,7 @@ func _set_web_collision_mask(new_value: int) -> void:
 
 # Sets the webbing state and fire any signals
 func _set_webbing(hand: WebHand, active: bool) -> void:
-	print("SET WEBBING", hand.side, active)
+	print("SET WEBBING: ", "side: ", hand.side, " active: ", active)
 	
 	# Skip if no change
 	if active == hand.active:
@@ -384,10 +337,3 @@ func _set_webbing(hand: WebHand, active: bool) -> void:
 		web_started.emit(hand.side)
 	else:
 		web_finished.emit(hand.side)
-
-
-# Updates our transform so we are positioned on our palm
-#func _update_transform() -> void:
-#	if hand_offset_mode != 4:
-#		transform = XRTools.get_palm_offset(hand_offset_mode, left_hand.controller)
-#		transform = XRTools.get_palm_offset(hand_offset_mode, right_hand.controller)
